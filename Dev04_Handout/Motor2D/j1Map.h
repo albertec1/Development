7990b214cdf933ad2703a817d1813143e78482c4
albertec1 <45201952+albertec1@users.noreply.github.com>
2019-10-03 10:28:20 +0200

    class4

---
+++ Dev04_Handout/Motor2D/j1Map.h
@@ -0,0 +1,120 @@
+#ifndef __j1MAP_H__
+#define __j1MAP_H__
+
+#include "PugiXml/src/pugixml.hpp"
+#include "p2List.h"
+#include "p2Point.h"
+#include "j1Module.h"
+#include "p2DynArray.h"
+
+
+// TODO 1: Create a struct for the map layer
+struct mapLayer
+{
+	p2SString			name;
+	unsigned int		width;
+	unsigned int		heigth;
+	unsigned int		gidArray;  //array to store all the id values1
+
+};
+// ----------------------------------------------------
+
+	// TODO 6: Short function to get the value of x,y
+
+
+
+// ----------------------------------------------------
+struct TileSet
+{
+	// TODO 7: Create a method that receives a tile id and returns it's Rect
+
+	p2SString			name;
+	int					firstgid;
+	int					margin;
+	int					spacing;
+	int					tile_width;
+	int					tile_height;
+	SDL_Texture*		texture;
+	int					tex_width;
+	int					tex_height;
+	int					num_tiles_width;
+	int					num_tiles_height;
+	int					offset_x;
+	int					offset_y;
+};
+
+enum MapTypes
+{
+	MAPTYPE_UNKNOWN = 0,
+	MAPTYPE_ORTHOGONAL,
+	MAPTYPE_ISOMETRIC,
+	MAPTYPE_STAGGERED
+};
+// ----------------------------------------------------
+struct MapData
+{
+	int					width;
+	int					height;
+	int					tile_width;
+	int					tile_height;
+	SDL_Color			background_color;
+	MapTypes			type;
+	p2List<TileSet*>	tilesets;
+	// TODO 2: Add a list/array of layers to the map!
+	p2List<mapLayer*>	layers;
+};
+
+// ----------------------------------------------------
+class j1Map : public j1Module
+{
+public:
+
+	j1Map();
+
+	// Destructor
+	virtual ~j1Map();
+
+	// Called before render is available
+	bool Awake(pugi::xml_node& conf);
+
+	// Called each loop iteration
+	void Draw();
+
+	// Called before quitting
+	bool CleanUp();
+
+	// Load new map
+	bool Load(const char* path);
+
+	//Load layer
+	bool LoadLayer(pugi::xml_node& node, mapLayer* layer);
+
+	// TODO 8: Create a method that translates x,y coordinates from map positions to world positions
+
+private:
+
+	bool LoadMap();
+	bool LoadTilesetDetails(pugi::xml_node& tileset_node, TileSet* set);
+	bool LoadTilesetImage(pugi::xml_node& tileset_node, TileSet* set);
+	//TODO 3: Create a method that loads a single laye
+	bool LoadLayer(pugi::xml_node& node, mapLayer* layer)
+	{
+		bool ret = false;
+		
+		/*layer->name.
+
+		return ret;*/
+	}
+
+public:
+
+	MapData data;
+
+private:
+
+	pugi::xml_document	map_file;
+	p2SString			folder;
+	bool				map_loaded;
+};
+
+#endif // __j1MAP_H__
